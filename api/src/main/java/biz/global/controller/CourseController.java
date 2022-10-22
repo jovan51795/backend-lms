7c06f3a38e54fca936a7902db8b60877d85e6fd1
nicamiranda <gennicamiranda@gmail.com>
2022-10-22 22:51:04 +0800

    added subject by course

--- api/src/main/java/biz/global/controller/CourseController.java
+++ api/src/main/java/biz/global/controller/CourseController.java
@@ -3,15 +3,20 @@ package biz.global.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 
 import biz.global.model.Course;
+import biz.global.model.ResponseModel;
 import biz.global.repo.CourseRepo;
+import biz.global.repo.SubjectRepo;
 
 @RestController
 @RequestMapping("api/courses/")
@@ -21,6 +26,15 @@ public class CourseController {
 	@Autowired
 	private CourseRepo courseRepo;
 	
+	@Autowired
+	private SubjectRepo subjectRepo;
+	
+	public ResponseEntity<ResponseModel> checker(List<Object> get){
+    	if(get.isEmpty()) {
+    		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ResponseModel(0, "No data exist", null, null));
+    	}
+    	 return ResponseEntity.ok().body(new ResponseModel(1, "Success", null, get));
+    }
 	
 	//////////////////////////////////////////////////////////////////////////////////  GET MAPPING  \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
     
@@ -31,7 +45,11 @@ public class CourseController {
         return course;
     }
     
-    
+    @GetMapping(value="getSubjectByCourse")
+    public ResponseEntity<ResponseModel> getAllSubjectbyCourse(@RequestParam Long id){
+    	List<Object> get = subjectRepo.getAllSubjectbyCourse(id);
+    	return checker(get);
+    }
     
 	//////////////////////////////////////////////////////////////////////////////////  POST MAPPING  \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
     
